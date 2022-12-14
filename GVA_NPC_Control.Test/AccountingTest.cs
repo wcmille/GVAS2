cabbbd3bc46d8fc791a8316c7115cb50fea8046d
Wayne Miller <wayne.c.miller@comcast.net>
2022-12-13 19:25:15 -0600

    Updated Beacon. Separated out profiles. Fixed thrusters.

--- GVA_NPC_Control.Test/AccountingTest.cs
+++ GVA_NPC_Control.Test/AccountingTest.cs
@@ -9,12 +9,34 @@ namespace GVA_NPC_Control.Test
         [TestMethod]
         public void TestMethod10_10_0()
         {
-            Accounting acct = new Accounting("Blue", 10, 10, 0);
+            Accounting acct = new Accounting(null,null,"Blue", 10, 10, 0);
             acct.TimePeriod();
 
             Assert.AreEqual(10, acct.Civilian);
             Assert.AreEqual(10, acct.Military);
             Assert.AreEqual(0.666, acct.UnspentUnits, 0.001);
+        }
+
+        [TestMethod]
+        public void TestMethod30_10_0()
+        {
+            Accounting acct = new Accounting(null, null, "Blue", 30, 10, 0);
+            acct.TimePeriod();
+
+            Assert.AreEqual(30, acct.Civilian);
+            Assert.AreEqual(10, acct.Military);
+            Assert.AreEqual(2, acct.UnspentUnits, 0.001);
+        }
+
+        [TestMethod]
+        public void TestMethod50_0_0()
+        {
+            Accounting acct = new Accounting(null, null, "Blue", 50, 0, 0);
+            acct.TimePeriod();
+
+            Assert.AreEqual(50, acct.Civilian, "Civilian");
+            Assert.AreEqual(0, acct.Military, "Military");
+            Assert.AreEqual(2, acct.UnspentUnits, 0.001, "Unspent");
         }
 
         [TestMethod]
