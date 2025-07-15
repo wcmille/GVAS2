47138240390192e3466f9da6c903bfadfb35f68e
Wayne Miller <wayne.c.miller@comcast.net>
2025-07-14 23:43:40 -0500

    Updated Economy. Set a small buffer on the SZ.

--- GVAS2Encounters/StarterModProject/LimitedProduction/LimitedProduction_SafeZone.cs
+++ GVAS2Encounters/StarterModProject/LimitedProduction/LimitedProduction_SafeZone.cs
@@ -8,11 +8,12 @@ namespace GVA.NPCControl.LimitedProduction
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_SafeZoneBlock), false)]
     public class LimitedProduction_SafeZoneBlock : LimitedProduction<IMySafeZoneBlock>
     {
-        BoundingSphereD blackZone = new BoundingSphereD(SharedConstants.WorldCenter, 400000.0);
-        BoundingSphereD blueZone = new BoundingSphereD(new Vector3D(97825.9, 79558.68, 5725255.17), 50000.0);
-        BoundingSphereD redZone = new BoundingSphereD(new Vector3D(124468.67, 191328.8, 5742379.16), 50000.0);
+        const double offset = 10000.0;
+        BoundingSphereD blackZone = new BoundingSphereD(SharedConstants.WorldCenter, offset + 400000.0);
+        BoundingSphereD blueZone = new BoundingSphereD(new Vector3D(97825.9, 79558.68, 5725255.17), offset + 50000.0);
+        BoundingSphereD redZone = new BoundingSphereD(new Vector3D(124468.67, 191328.8, 5742379.16), offset + 50000.0);
         BoundingSphereD whiteZone = new BoundingSphereD(new Vector3D(-1854042, 131072, 2421365), 200000.0);
-        BoundingSphereD greenZone = new BoundingSphereD(new Vector3D(111869, 136079, 5673610), 35000.0);
+        BoundingSphereD greenZone = new BoundingSphereD(new Vector3D(111869, 136079, 5673610), offset + 35000.0);
         public override bool Illegal()
         {
             Vector3D pos = productionBlock.GetPosition();
